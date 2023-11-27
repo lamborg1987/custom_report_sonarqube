d9e8f2b41ff106ed8c8ba236c3bb1efc34c916e1
Jorge Uztariz <60784262+lamborg1987@users.noreply.github.com>
2023-11-27 14:44:41 -0500

    Delete pack_linux.py

--- pack_linux.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="CustomReport",
-    version="1.0",
-    description="Custom Report for sonarqube",
-    executables=[Executable("run.py")],
-)
